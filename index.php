ef6535a6a010bd2c07c4488fada7bb1e18d4a14d
SantanuPaul04 <paulsantanu968@gmail.com>
2023-08-21 22:20:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first html code</title>
+</head>
+<body>
+    <h1>My First GitHub Push</h1>
+    <?php
+    echo "My Name is Santanu Paul <br>"; 
+        print "Hi, I am a Bca Student <br>";
+
+        $a=10;
+        $b=20;
+        echo "<br>";
+        echo "$a <br>" ;
+        echo "$b <br>";
+        echo $a + $b."<br>";
+        echo $b - $a."<br>";
+        echo ++$a."<br>";
+        echo --$b;
+    ?> 
+    
+</body>
+</html>
